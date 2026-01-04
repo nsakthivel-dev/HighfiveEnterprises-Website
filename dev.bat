19053e856b16c8f15d66260b525d44acaea208aa
nsakthivel-dev <nsakthiveldev@gmail.com>
2026-01-04 16:15:32 +0530

    Remove unwanted files: batch files and redundant documentation

--- dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Highfive Enterprises Website in Development Mode...
-echo ========================================================
-
-echo Starting development server...
-pnpm dev
-
-pause
--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Highfive Enterprises Website...
-echo =====================================
-
-echo Make sure you have built the project first using build.bat
-echo Starting the server...
-node dist/index.js
-
-pause
