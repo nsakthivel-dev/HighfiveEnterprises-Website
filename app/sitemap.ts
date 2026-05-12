506d4168e604a78d1622c3fbc394b2988ba6e030
nsakthivel-dev <nsakthiveldev@gmail.com>
2026-05-12 13:12:59 +0530

    Redirect Issue is Fixed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next' 
  
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap { 
   return [ 
     { url: 'https://www.lupusventure.com', lastModified: new Date(), changeFrequency: 'monthly', priority: 1 }, 
